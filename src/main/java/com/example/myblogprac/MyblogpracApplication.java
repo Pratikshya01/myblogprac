0369d0955d6c3b8cb98fc67d1696fd99e37d83d2
Pratikshya <pratikshyagochhayat@gmail.com>
2024-07-19 23:07:11 +0530

    127:Create save Post Feature

---
+++ src/main/java/com/example/myblogprac/MyblogpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.myblogprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyblogpracApplication.class, args);
+	}
+
+}
